69400eade1fc57bc823a4e8bf9eaad6e21186174
kobiregev <regev79@gmail.com>
2022-07-07 18:31:23 +0300

    chore: render lists dynamicly

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,64 @@
 import setupDnd from "./dragAndDrop/dragAndDrop";
 import { dragEndEvent } from "./dragAndDrop/types";
+import { List, Task } from "./types";
+import { v4 as uuidv4, v4 } from "uuid";
 import "./style.css";
-const app = document.querySelector<HTMLDivElement>("#app")!;
 
-const onDragComplete = (event: dragEndEvent) => {
-  console.log(event);
+const lanes = document.querySelector<HTMLDivElement>(".lanes");
+const defaultLists: List[] = [
+  {
+    id: uuidv4(),
+    tasks: [{ id: v4(), body: "Finish this trello clone" }],
+    title: "Backlog",
+  },
+  {
+    id: uuidv4(),
+    tasks: [
+      { id: v4(), body: "Upload To the web" },
+      { id: v4(), body: "Learn GraphQl" },
+    ],
+    title: "To Do Today",
+  },
+  { id: uuidv4(), tasks: [], title: "Done Today" },
+];
+const setupLists = () => {
+  const lists = localStorage.getItem("lists");
+  return lists ? JSON.parse(lists) : defaultLists;
+};
+const createTask = (task: Task) => {
+  const element = document.createElement("div");
+  element.dataset.draggable = "";
+  element.classList.add("task");
+  element.innerText = task.body;
+  return element;
 };
 
-setupDnd(onDragComplete);
+const renderLists = (lists: List[]) => {
+  lists.forEach((list) => {
+    const listElement = document.createElement("div");
+    const headerElement = document.createElement("div");
+    const tasksElement = document.createElement("div");
 
-// app.innerHTML = `
-// <div class="lanes">
-// <div class="lane">
-//   <div class="header">
-//     Backlog
-//   </div>
-//   <div class="tasks">
-//     <div class="task">
-//       Do Laundry
-//     </div>
-//     <div class="task">
-//       Edit Video
-//     </div>
-//   </div>
-//   <form>
-//     <input class="task-input" type="text" placeholder="Task Name" />
-//   </form>
-// </div>
-// <div class="lane">
-//   <div class="header">
-//     Doing
-//   </div>
-//   <div class="tasks">
-//     <div class="task">
-//       Record Video
-//     </div>
-//   </div>
-//   <form>
-//     <input class="task-input" type="text" placeholder="Task Name" />
-//   </form>
-// </div>
-// <div class="lane">
-//   <div class="header">
-//     Done
-//   </div>
-//   <div class="tasks">
-//     <div class="task">
-//       Plan Trello Clone Video
-//     </div>
-//   </div>
-//   <form>
-//     <input class="task-input" type="text" placeholder="Task Name" />
-//   </form>
-// </div>
-// </div>
-// `;
+    listElement.classList.add("lane");
+    headerElement.classList.add("header");
+    headerElement.innerText = list.title;
+    tasksElement.classList.add("tasks");
+    tasksElement.dataset.dropZone = "";
+    listElement.append(headerElement, tasksElement);
+
+    list.tasks?.forEach((task) => {
+      const element = createTask(task);
+      tasksElement.appendChild(element);
+    });
+
+    lanes?.appendChild(listElement);
+  });
+};
+
+const lists: List[] = setupLists();
+
+const onDragComplete = (event: dragEndEvent) => {
+};
+
+renderLists(lists);
+setupDnd(onDragComplete);
